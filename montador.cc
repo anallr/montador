5e008f850acaff0eceaf2ed85a8df010c3de5d45
tomtomesteves <thomasesteves@hotmail.com>
2017-05-15 12:14:38 -0300

    Finalizado

--- montador.cc
+++ montador.cc
@@ -5,22 +5,8 @@
 #include "instruction.h"
 #include "assembler.h"
 using namespace std;
-int main(int argc, char *args[]) {
-  //Avaliação dos parametros
-  enum arg_type {o, v, outname, inname, noarg};
-  arg_type args_type[5];
-  for (int i = 0; i < 5; ++i)
-    args_type[i] = inname;
-  bool verbose = false;
-  for (int i = 1; i < argc; ++i) {
-    if (!strcmp(args[i], "-v")) {
-      verbose = true;
-      args_type[i] = v;
-    } else if (!strcmp(args[i], "-o")) {
-      args_type[i] = o;
-      args_type[i + 1] = outname;
-    }
-  }
+int main(int argc, char *args[]) {  
+  //FILE INPUT AND output
   const char *in_filename = NULL,
              *out_filename = NULL;
   for (int i = 1; i < argc; ++i)
@@ -37,8 +23,6 @@ int main(int argc, char *args[]) {
     str->replace(str->find("asm"), 3, "sa");
     out_filename = str->c_str();
   }
-  //printf("IN FILE: %s\n", in_filename);
-  //printf("OUT FILE: %s\n", out_filename);
   //Processo de montagem
   Assembler assembler(in_filename, out_filename);
   assembler.firstPass();
